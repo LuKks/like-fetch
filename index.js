ff0b9a7ed93ea93c8266cac97de3be0673d59749
Lucas Barrena <lucasbarrena98@gmail.com>
2024-01-22 15:08:36 -0300

    Custom error for when request fails

--- index.js
+++ index.js
@@ -8,45 +8,29 @@ function fetch (url, options = {}) {
   const retryOptions = remove(opts, 'retry')
   const timeout = remove(opts, 'timeout')
   const validateStatus = remove(opts, 'validateStatus')
-  // const proxy = remove(opts, 'proxy')
   const requestType = remove(opts, 'requestType')
   const responseType = remove(opts, 'responseType')
+  const signal = remove(opts, 'signal')
 
-  // const agent = handleProxyAgent(opts, proxy)
   handleRequestTypes(opts, requestType)
   handleResponseTypes(opts, responseType)
 
-  let abortController = null
-  let timeoutId = null
+  let timeoutSignal = handleTimeout(opts, timeout)
 
   const promise = new Promise(callback)
-
-  // by now they're not null:
-  promise.controller = abortController.controller
-  promise.timeoutId = timeoutId
-
+  promise.controller = new AbortController()
   return promise
 
-  // + this function is not as clean as I like and there is too much going on but it's difficult due:
-  // doing yielded retries, exposing properly the controller for React and wanting make it work for all use cases
   async function callback (resolve, reject) {
-    try {
-      abortController = handleAbortController(opts)
-      timeoutId = handleTimeout(opts, timeout, abortController)
-    } catch (error) {
-      reject(error)
-      return
-    }
-
     for await (const backoff of retry(retryOptions)) {
       try {
-        const response = await cfetch(url, { signal: abortController.signal/* , agent */, ...opts })
+        const signals = AbortSignal.any([signal, timeoutSignal, promise.controller.signal].filter(s => s))
+        const response = await cfetch(url, { ...opts, signal: signals })
 
         handleValidateStatus(response, validateStatus)
 
         if (responseType === 'json' || responseType === 'text') {
           const body = await response[responseType]()
-          clearTimeout(promise.timeoutId)
           resolve(body)
         } else {
           resolve(response)
@@ -55,19 +39,22 @@ function fetch (url, options = {}) {
         return
       } catch (error) {
         // manual abort like at React useEffect cleanup, so it must not retry
-        if (error.name === 'AbortError' && !promise.controller.$timedout) {
+        if ((error.name === 'AbortError' || error.name === 'TimeoutError') && !(timeoutSignal && timeoutSignal.aborted)) {
           reject(error)
           return
         }
 
-        // error.name => 'AbortError', error.message => 'The user aborted a request.'
-        // + should not clear/abort if it's from validateStatus?
-
-        if (backoff.left > 0) {
-          clearTimeout(promise.timeoutId)
-          if (promise.controller) {
-            promise.controller.abort()
+        if (error.response) {
+          if (responseType === 'json' || responseType === 'text') {
+            try {
+              error.data = await error.response[responseType]()
+            } catch {}
           }
+        }
+
+        if (error.name === 'LikeFetchError') {
+          reject(error)
+          return
         }
 
         try {
@@ -77,28 +64,12 @@ function fetch (url, options = {}) {
           return
         }
 
-        abortController = handleAbortController(opts)
-        promise.controller = abortController.controller
-        promise.timeoutId = handleTimeout(opts, timeout, abortController)
+        promise.controller = new AbortController()
+        timeoutSignal = handleTimeout(opts, timeout)
       }
     }
   }
 }
-
-// "https-proxy-agent" is for Node.js but obviously you won't use fetch() with the proxy option in the browser
-// ie. React analyzes all the "require"s even if the code doesn't reach there, and in this case crashes due Node dependencies
-// so disabling this for now
-/* function handleProxyAgent (opts, proxy) {
-  if (!proxy) return
-  if (opts.agent) throw new Error('Conflict having both opts.proxy and opts.agent, only one allowed')
-
-  // + add support for passing an object { host, port, auth: { username, password } }
-  // + add support for socks5
-  const HttpsProxyAgent = require('https-proxy-agent')
-  const agent = new HttpsProxyAgent(proxy)
-
-  return agent
-} */
 
 function handleRequestTypes (opts, requestType) {
   if (!requestType) return
@@ -138,59 +109,52 @@ function handleResponseTypes (opts, responseType) {
   throw new Error('responseType not supported (' + responseType + ')')
 }
 
-function handleAbortController (opts) {
-  // signals can't be reused, so if the user passed in one then it won't be used again
-  const controllerOpt = remove(opts, 'controller')
-  const signalOpt = remove(opts, 'signal')
+function handleTimeout (opts, timeout) {
+  if (typeof timeout !== 'number' || timeout === 0) return null
 
-  if (controllerOpt) {
-    if (signalOpt && controllerOpt.signal !== signalOpt) {
-      throw new Error('If you pass your own controller and signal, they have to be the same (opts.controller.signal === opts.signal)')
-    }
-    return { controller: controllerOpt, signal: controllerOpt.signal, custom: true }
-  }
-
-  if (signalOpt) {
-    if (controllerOpt && controllerOpt.signal !== signalOpt) {
-      throw new Error('If you pass your own controller and signal, they have to be the same (opts.controller.signal === opts.signal)')
-    }
-    return { controller: controllerOpt || null, signal: signalOpt, custom: true }
-  }
-
-  const controller = new AbortController()
-  return { controller, signal: controller.signal }
-}
-
-function handleTimeout (opts, timeout, abortController) {
-  if (timeout === undefined || timeout === 0) return
-  if (abortController.custom && !abortController.controller) throw new Error('Conflict having both opts.timeout and opts.signal, only one allowed or add opts.controller')
-
-  const controller = abortController.controller // to keep the same variable reference
-
-  const timeoutId = setTimeout(() => {
-    controller.$timedout = true
-    controller.abort()
-  }, timeout)
-
-  if (timeoutId.unref) timeoutId.unref()
-
-  controller.signal.addEventListener('abort', () => clearTimeout(timeoutId), { once: true })
-
-  return timeoutId
+  return AbortSignal.timeout(timeout)
 }
 
 function handleValidateStatus (response, validateStatus) {
   if (!validateStatus) return
 
-  // + should throw custom error like AxiosError
   if (typeof validateStatus === 'number') {
-    if (validateStatus !== response.status) throw response
+    if (validateStatus !== response.status) throw customError(response.status, response)
   } else if (typeof validateStatus === 'function') {
-    if (!validateStatus(response.status)) throw response
+    if (!validateStatus(response.status)) throw customError(response.status, response)
   } else if (validateStatus === 'ok') {
-    if (!response.ok) throw response
+    if (!response.ok) throw customError(response.status, response)
   } else {
     throw new Error('validateStatus not supported (' + validateStatus + ')')
+  }
+}
+
+function customError (status, response) {
+  const message = 'Request failed with status code ' + status
+
+  if (status >= 400 && status < 499) {
+    return new LikeFetchError(message, 'ERR_BAD_REQUEST', response)
+  }
+
+  if (status >= 500 && status < 599) {
+    return new LikeFetchError(message, 'ERR_BAD_RESPONSE', response)
+  }
+
+  return new LikeFetchError(message, undefined, response)
+}
+
+class LikeFetchError extends Error {
+  constructor (msg, code, response) {
+    super(msg)
+    this.code = code
+
+    this.cause = null
+    this.response = response
+    this.data = undefined
+  }
+
+  get name () {
+    return 'LikeFetchError'
   }
 }
 
